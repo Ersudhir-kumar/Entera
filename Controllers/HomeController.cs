aef2e63aed20314208dc52e71a434c48387069b5
hp <hp@LAPTOP-014CICSL>
2026-01-01 18:40:13 +0530

    gora

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,11 @@ namespace Entera.Controllers
 		}
 
 
+		public IActionResult Gora()
+		{
+			return View();
+		}
+
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
