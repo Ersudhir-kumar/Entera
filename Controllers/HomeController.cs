a7b86b54d44605a6b09762767646e8cfa14fbc66
hp <hp@LAPTOP-014CICSL>
2026-01-01 17:42:00 +0530

    SuperAdminDashboard

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,7 +16,14 @@ namespace Entera.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Dashboard()
+		{
+			return View();
+		}
+
+
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
